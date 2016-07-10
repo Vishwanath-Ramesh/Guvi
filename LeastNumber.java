4b538836d30b1cdd89bb89e3ade39596b76de04a
Vishwanath95 <vishwanath95.kpr@gmail.com>
2016-07-10 20:00:36 +0530

    Add files via upload

---
+++ LeastNumber.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class LeastNumber 
+{
+    public static void main(String[] args) 
+    {
+        int n, temp;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number of digits:");
+        n = s.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter the "+n+" elements:");
+        for (int i = 0; i < n; i++) 
+        {
+            a[i] = s.nextInt();
+        }
+        for(int i = 0;i<n;i++){
+			for(int j = i+1;j<n;j++){
+				if(a[i] > a[j]){
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+        System.out.printf("The numbers in Ascending Order are:");
+        for (int i = 0; i < n ; i++) 
+        {
+            System.out.print(a[i]);
+        }
+		System.out.printf("\nEnter the number of digits to delete:");
+		int d = s.nextInt();
+		System.out.printf("\nThe Remaining numbers are:");
+		for(int x = 0;x<n-d;x++){
+			System.out.print(a[x]);
+		}
+    }
+}
