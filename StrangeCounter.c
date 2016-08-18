a79beb587e129462c9a225d9e0023dcda07fc32e
Vishwanath95 <vishwanath95.kpr@gmail.com>
2016-08-18 13:19:27 +0530

    Add files via upload

---
+++ StrangeCounter.c
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+int main(){
+    int value[10];
+    int time,num = 3,t = 0;
+    for(time = 1;time < 10;time++){
+        while(num >= 1){
+            if(num == 1){
+              value[time]=num;
+                num = value[time]+3;
+              }
+            else{
+              value[time]=num;
+            }
+            num--;
+            break;
+    }
+        }
+    printf("Enter the time\n");
+    scanf("%d",&t);
+    printf("The value at time %d is %d\n",t,value[t]);
+    return 0;
+}
+
