563d6e537ff54c860324a60ea8b3b5d82dbf2c58
Vishwanath <vishwanath95.kpr@gmail.com>
2016-11-04 20:43:41 +0530

    Guvi Hunter Set 12 - 112

---
+++ TheKickerDog.c
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+int main(){
+    int turns = 1,distance=1,value;
+    scanf("%d",&value);
+    while(1){
+        if(turns==1){
+           distance=1;
+           turns++;}
+        else{
+        distance += distance;
+        turns++;}
+        if(distance==value){
+            printf("True");
+            printf("\nThe Dog reaches the end at %d Jumps/Turns.",turns-1);
+            break;}
+        else if(distance > value){
+            printf("False");
+            break;}
+    }
+    return 0;
+}
+
