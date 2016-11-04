4bbc283a03ea95bf03a4c6057cf9a1974045dcb0
Vishwanath <vishwanath95.kpr@gmail.com>
2016-11-04 20:40:25 +0530

    Guvi Hunter Set 10 - 97

---
+++ DogCode.java
@@ -0,0 +1,19 @@
+﻿import java.util.*;
+class DogCode
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		int smile=1,cry=2,fear=3;
+		String string = new String(scanner.nextLine());
+		String[] string_array = string.split(" ");
+		for(String str:string_array){
+			if(str.equalsIgnoreCase("smile"))
+				System.out.print(smile);
+			else if(str.equalsIgnoreCase("cry"))
+				System.out.print(cry);
+			else if(str.equalsIgnoreCase("fear"))
+				System.out.print(fear);
+		}
+	}
+}
